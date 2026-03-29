927e30a70dbcd542b3cca114b4e789403c293985
David de Leon <johndaviddeleon18@gmail.com>
2026-03-28 22:30:41 -0600

    Update db.js

--- bookapro/backend/config/db.js
+++ bookapro/backend/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-//DB Connectionnn
+//DB Connectionnnn
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
